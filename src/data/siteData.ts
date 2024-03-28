42568141fedb94451b6f178c1e31796d63385c59
Jaime Aleman <jjaimealeman@gmail.com>
2024-03-27 21:04:11 -0600

    feat: update siteData with URL and better description.

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -1,5 +1,33 @@
 export const baseData = {
-  title: "jaime portfolio test",
-  description:
-    "jaime description test jaime description test jaime description test ",
+  URL: "https://jjaimealeman.com",
+  title: "Jaime Aleman",
+  description: "Freelance Web Developer, Graphic Designer - based in El Paso, TX.",
 };
+
+export const socialData = [
+  {
+    title: "dribbble",
+    href: "https://dribbble.com/",
+    icon: "icon--dribbble",
+  },
+  {
+    title: "instagram",
+    href: "https://instagram.com/",
+    icon: "icon--instagram",
+  },
+  {
+    title: "twitter",
+    href: "https://twitter.com/",
+    icon: "icon--twitter",
+  },
+  {
+    title: "youtube",
+    href: "https://youtube.com/",
+    icon: "icon--youtube",
+  },
+  {
+    title: "github",
+    href: "https://github.com/",
+    icon: "icon--github",
+  },
+];
