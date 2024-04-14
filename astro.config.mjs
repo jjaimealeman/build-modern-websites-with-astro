562001f1a65510864b4baae069ff0889dbccd8e3
Jaime Aleman <jjaimealeman@gmail.com>
2024-04-13 20:50:43 -0600

    feat: install sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import netlify from "@astrojs/netlify";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: { prefetchAll: true },
-  integrations: [tailwind(), icon()],
+  prefetch: {
+    prefetchAll: true
+  },
+  integrations: [tailwind(), icon(), sitemap()],
   output: "hybrid",
   adapter: netlify()
 });
