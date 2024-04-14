c27aadf258eda9b9b984c943724149b1bf4e2399
Jaime Aleman <jjaimealeman@gmail.com>
2024-04-13 20:57:59 -0600

    feat: update sitemap URL path.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://build-modern-websites-with-astro.netlify.app/',
   prefetch: {
     prefetchAll: true
   },
