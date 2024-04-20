a31cb23001c1f0ee5aeb79ae7833dedddb89b898
Jaime Aleman <jjaimealeman@gmail.com>
2024-04-20 14:22:26 -0600

    fix: comment out netlify in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import netlify from "@astrojs/netlify";
+// import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
 
 import node from "@astrojs/node";
