4a7e5392980aa6f3ec544bf88f87bff746ccba11
Jaime Aleman <jjaimealeman@gmail.com>
2024-05-10 16:40:15 -0600

    add db/seed

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,25 @@
-import { db } from 'astro:db';
+import { Links, db } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  await db.insert(Links).values([
+    {
+      title: "Title",
+      url: "google.com",
+      isRead: true,
+      upvoteNum: 3,
+    },
+    {
+      title: "Another Title",
+      url: "example.com",
+      isRead: false,
+      upvoteNum: 5,
+    },
+    {
+      title: "Yet Another Title",
+      url: "github.com",
+      isRead: true,
+      upvoteNum: 10,
+    },
+  ]);
 }
